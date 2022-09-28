cb02230f0a73854f9afedd29e52cbb0db853416b
SayedZubair <sayedzubairhashimi2020>
2022-09-27 17:07:56 -0700

    third commit

--- src/Class04B14/Class03.java
+++ src/Class04B14/Class03.java
@@ -6,5 +6,6 @@ public static void main(String[] args) {
 	
 	System.out.println("hey this is a git class");
 	System.out.println("hey this is a change in my project ");
+	System.out.println("how are u this is a new version ");
 }
 }
